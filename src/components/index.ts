cc52ac504a04cfdb1dccc44474fe8eac8dd0cb97
el-makarova <el-makarova@yandex-team.ru>
2022-01-20 11:08:57 +0300

    fix: Toaster should be exported

--- src/components/index.ts
+++ src/components/index.ts
@@ -28,6 +28,7 @@ export * from './Switch';
 export * from './Table';
 export * from './Tabs';
 export * from './TextInput';
+export * from './Toaster';
 export * from './User';
 export * from './UserAvatar';
 
